9d78ad16365f35390d666776e976714cf24cef23
rachael3001 <123117247+rachael3001@users.noreply.github.com>
2023-10-16 10:31:09 +0100

    test

--- Code.py
+++ Code.py
@@ -1,1 +1,13 @@
-print("Hello")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("airline_delay.csv")
+
+print(df.head())
+
+delays = ["nas_ct","security_ct","late_aircraft_ct", "weather_ct","carrier_ct"]
+
+sns.distplot(df[delays], bins=30, kde=False,color='red')
+plt.xlim([0, 200]) 
+plt.show()
