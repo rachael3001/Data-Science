ed15206c7216febf876a893b02afc6a50ef1c941
rachael3001 <123117247+rachael3001@users.noreply.github.com>
2023-10-16 14:22:07 +0100

    Updated code
    Added comments for readability

--- Code.py
+++ Code.py
@@ -5,19 +5,21 @@ import matplotlib.ticker as mtick
 
 df = pd.read_csv("airline_delay.csv")
 
-#print(df.head())
 
 y = df["arr_flights"]
 x = df["arr_del15"]
 delays  = ["nas_ct","security_ct","late_aircraft_ct", "weather_ct","carrier_ct"]
 
+#Creation of data 
 df["flightdelays_arrivals"] = x/y
+
+#plotting graph
 plot = sns.histplot(df["flightdelays_arrivals"])
-#sns.boxenplot(x=x, y=y,width_method="linear")
-# plt.ylim(0,7500)
 plt.ylabel("Frequency")
 plt.xlabel("Percent of flights delayed at an airport")
 plt.title("Distribution of percentage of flights delayed out of total Flights arriving")
+
+#Converting x-axis to percents for readability 
 plot.xaxis.set_major_formatter(mtick.PercentFormatter())
 vals = plot.get_xticks()
 plot.set_xticklabels(['{:,.0%}'.format(x) for x in vals])
